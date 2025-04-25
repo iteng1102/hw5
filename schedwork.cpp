3c69624be9ae5e67ce554f21e21fb80eed061373
Hsiao Jung Teng <iteng@usc.edu>
2025-04-25 05:38:24 +0000

    pushed work

--- schedwork.cpp
+++ schedwork.cpp
@@ -21,6 +21,9 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 
 // Add prototypes for any helper functions here
+bool search(int currentDay, size_t daily, size_t maxShift, size_t worker, std::vector<size_t>& currentShift, DailySchedule& sched, const AvailabilityMatrix& avail);
+bool allComplete(DailySchedule schedule, const size_t dailyNeed);
+bool isValid(DailySchedule schedule, size_t worker, size_t currentDay, size_t maxShift, const AvailabilityMatrix avail, std::vector<size_t> workerShift);
 
 
 // Add your implementation of schedule() and other helper functions here
@@ -37,9 +40,61 @@ bool schedule(
     }
     sched.clear();
     // Add your code below
+    std::vector<size_t> workerShift(avail[0].size()); //make a new vector to keep track of worker shifts 
+    for (size_t i = 0; i < avail[0].size(); i++){ //set them all to 0 first 
+      workerShift[i] = 0;
+    }
+    sched.resize(avail.size()); //resize schedule to fit avail 
+    if (!(search(0, dailyNeed, maxShifts, 0, workerShift, sched,avail))){ //return false if no avaiable schedule 
+        return false; 
+    }
+    return true; 
+}
 
+bool allComplete(DailySchedule schedule, const size_t dailyNeed){ //check if the shedule is complite 
+    //std::cout << "allComplete checked" << std::endl;
+    if (schedule.empty()==true){ //return false if empty 
+        return false; 
+    }
+    for (DailySchedule::iterator day = schedule.begin(); day!=schedule.end(); ++day){ //go through schedule and check if day's shifts align with need 
+        std::vector<Worker_T> currentDay = *day; 
+        if(currentDay.size()!=dailyNeed){ //return fasle if not align
+            return false; 
+        }
+    }
+    return true; 
+}
 
+bool isValid(DailySchedule schedule, size_t worker, size_t currentDay, size_t maxShift,const AvailabilityMatrix avail, std::vector<size_t> workerShift)
+{
+    //std::cout << "Isvalid checked" << std::endl;
+    if (avail[currentDay][worker]== 1 && workerShift[worker]<maxShift){ //checks if the worker is avaiable & if their  shift is less than max (constraints)
+        return true; 
+    }
+    return false; 
+}
 
+bool search(int currentDay, size_t daily, size_t maxShift, size_t worker, std::vector<size_t>& currentShift, DailySchedule& sched, const AvailabilityMatrix& avail)
+{
+    if (allComplete(sched, daily)){ //base case and return true if all complete 
+        return true; 
+    }
+    if (sched[currentDay].size()== daily){ //move on to next day if the day is full 
+      return search(currentDay+1, daily, maxShift, 0, currentShift, sched, avail); //start with worker 0
+    }
 
+    for (size_t w = worker; w < avail[currentDay].size(); ++w){ //iterate through the workers 
+      if (isValid(sched, w, currentDay, maxShift,avail, currentShift)){ 
+        sched[currentDay].push_back(w); //if the worker is valid to work then push it into schedule 
+        currentShift[w] +=1;  //update the shift vector to keep track of worker 
+        bool status = search(currentDay, daily, maxShift, worker + 1, currentShift, sched, avail); //checks if still valid with next worker 
+        if (status){ 
+          return true; 
+        }
+        sched[currentDay].pop_back(); //backtrack and undo if not working
+        currentShift[w] -= 1; //also subtract the worker from the shift
+      }
+    }
+    return false; 
 }
 
--- wordle.cpp
+++ wordle.cpp
@@ -13,6 +13,13 @@ using namespace std;
 
 
 // Add prototypes of helper functions here
+void helperWordle(
+    const std::string&in,
+    std::string word,
+    size_t index,
+    const std::string floating, 
+    const std::set<std::string>& dict,  
+    std::set<std::string>& result);
 
 
 // Definition of primary wordle function
@@ -22,7 +29,54 @@ std::set<std::string> wordle(
     const std::set<std::string>& dict)
 {
     // Add your code here
+    std::set<std::string> result;
+    helperWordle(in, "", 0, floating, dict, result);
+    return result;
 
 }
 
 // Define any helper functions here
+void helperWordle (
+    const std::string&in,
+    std::string word,
+    size_t index,
+    const std::string floating, 
+    const std::set<std::string>& dict,  
+    std::set<std::string>& result)
+{
+
+  if (index == in.length()){ //checks for base case when the index reaches end of string 
+    if (dict.find(word)!=dict.end() && floating.empty()==true){ //if the word is in the dictionary and all floating letters were used 
+      result.insert(word); //insert word in result 
+    }
+    return;
+  }
+
+  if (in[index]!='-'){
+    helperWordle (in, word + in[index], index +1, floating, dict, result); //recurse if we know the letter and add it to the word 
+  }
+  else{
+    char alphabet[] = "abcdefghijklmnopqrstuvwxyz"; 
+    size_t remain = 0; 
+    for (size_t i = index; i< in.length(); i++){ //calculate remaining dashes to see if floating fits 
+      if (in[i]=='-'){
+        remain++;
+      }
+    }
+    for (int i = 0; i< 26; i++){ //goes through the alphabet if its a dash 
+      char letter = alphabet[i];
+      size_t pos = floating.find(letter); //check if the letter is in our floating 
+      if (pos!= std::string::npos){ //if its in floating, delete it from floating
+        //floating.erase(pos,1); old code
+        std::string newFloating = floating; //make new floating cuz its constant  
+        newFloating.erase(pos,1); //erase it from new floating 
+        helperWordle(in, word + letter, index + 1, newFloating, dict, result); //recurse again after adding floating to word and go to next index with new floating  
+      }
+      else if (remain > floating.length()){ //add letter to word if there's space if considering other floating letters 
+        helperWordle(in, word + letter, index + 1, floating, dict, result); //recurse again with next index
+      }
+    }
+  }
+
+
+}
